78c6a57e34833d22675dcd4fa35c1de313288c29
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2024-11-19 21:46:05 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import { AuthContext } from '../context/AuthContext';
 
 const Header = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
     <header className="bg-black text-white p-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <Link to="/">
-          <img 
-            src={logo} 
-            alt="Easwari Engineering College Logo" 
-            className="w-25 h-12"
-          />
+            <img 
+              src={logo} 
+              alt="Easwari Engineering College Logo" 
+              className="w-25 h-12"
+            />
           </Link>
         </div>
         <nav className="flex space-x-6">
@@ -20,7 +23,9 @@ const Header = () => {
           <Link to="/events" className="hover:text-gray-300 transition-colors duration-300">Events</Link>
           <Link to="/openings" className="hover:text-gray-300 transition-colors duration-300">Jobs</Link>
           <Link to="/ats-tracker" className="hover:text-gray-300 transition-colors duration-300">ATS Tracker</Link>
-          <Link to="/auth" className="hover:text-gray-300 transition-colors duration-300">Login</Link>
+          {!isAuthenticated && (
+            <Link to="/auth" className="hover:text-gray-300 transition-colors duration-300">Login</Link>
+          )}
         </nav>
       </div>
     </header>
