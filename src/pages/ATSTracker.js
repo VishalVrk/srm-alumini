8f543c23d88c7d6929584e5b9b34e6ac413dcb0b
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2024-11-19 22:03:44 +0530

    Update ATSTracker.js

--- src/pages/ATSTracker.js
+++ src/pages/ATSTracker.js
@@ -22,7 +22,7 @@ function ATSTracker() {
             frameBorder="0"
             style={{
               position: "relative",
-              top: "-150px", // Shift content up to hide the header
+              top: "-165px", // Shift content up to hide the header
               width: "100%",
               height: "850px",
               border: "none",
